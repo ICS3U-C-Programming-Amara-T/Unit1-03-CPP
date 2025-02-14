1bf2f3ece5fe9e31443633f0f411ac019be399e4
Amara Tie <amara.tie@stu.ocsb.ca>
2025-02-14 05:06:23 +0000

    Learned to relay messages in CPP format

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2025 Amara Tie All rights reserved.
+
+//
+
+// Created by: Amara Tie 
+
+// Date: Feb 13, 2025
+
+// This program will illustrate how to say Hello World with style in CPP.
+
+#include <iostream>
+
+int main(){
+    std::cout << " cpplint helloWorldStyle.cpp" << std::endl;
+}
